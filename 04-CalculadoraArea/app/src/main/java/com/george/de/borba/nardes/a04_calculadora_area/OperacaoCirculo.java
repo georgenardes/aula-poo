0f7e02b2a831b91ae9e243bb55a62b30446539fb
georgenardes <georgenardes@gmail.com>
2018-10-22 21:36:45 -0300

    Initial Commit

---
+++ 04-CalculadoraArea/app/src/main/java/com/george/de/borba/nardes/a04_calculadora_area/OperacaoCirculo.java
@@ -0,0 +1,13 @@
+package com.george.de.borba.nardes.a04_calculadora_area;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OperacaoCirculo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_operacao_circulo);
+    }
+}
